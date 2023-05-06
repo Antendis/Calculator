7d433d41cfe82ae89e87525f24938170414d55fa
Antendis <86629893+Antendis@users.noreply.github.com>
2023-05-06 21:23:24 +0100

    operator selection works as intended

--- CalculatorV1/calculator.py
+++ CalculatorV1/calculator.py
@@ -1,12 +1,13 @@
 print(" \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n")
 
-operator = input("1. add\n2. subtract\n3. multiply\n4. divide\nwhat would you like to do? (1/2/3/4): \n")
+operator = input("1. add\n2. subtract\n3. multiply\n4. divide\nwhat would you like to do? (1/2/3/4):\n")
 
 def getOperator():
-    if operator == 1 or 2 or 3 or 4:
+    global operator
+    if operator in ["1", "2", "3", "4"]:
         print("success")
     else:
-        input("not a valid option, please try again: ")
-        getOperator
+        operator = input("not a valid option, please try again:\n")
+        getOperator()
 
 getOperator()
