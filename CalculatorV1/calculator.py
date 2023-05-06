1b377dde43cfbe2c44c4e4dab500059a285994c2
Antendis <86629893+Antendis@users.noreply.github.com>
2023-05-06 23:09:29 +0100

    Completed - command line calculator
    may need to make a pop up version next with buttons

--- CalculatorV1/calculator.py
+++ CalculatorV1/calculator.py
@@ -1,13 +1,21 @@
-print(" \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n")
-
-operator = input("1. add\n2. subtract\n3. multiply\n4. divide\nwhat would you like to do? (1/2/3/4):\n")
+symbols = ["+", "-", "*", "/"]
+operator = input("1. add\n2. subtract\n3. multiply\n4. divide\nwhat would you like to do? (1/2/3/4): ")
 
 def getOperator():
-    global operator
+    global operator, firstNumber, secondNumber
     if operator in ["1", "2", "3", "4"]:
-        print("success")
+
+        firstNumber = input("first number: ")
+        secondNumber = input("second number: ") 
+
+        operator = symbols[int(operator)-1]
+        print(eval(f"{firstNumber} {operator} {secondNumber}"))
+
+        if operator == 1:
+            print(firstNumber)
+
     else:
-        operator = input("not a valid option, please try again:\n")
+        operator = input("not a valid option, please try again: ")
         getOperator()
 
 getOperator()
