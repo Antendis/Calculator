9c5d2a122962c32e2a29e2dacc2090ea2ef75666
Antendis <86629893+Antendis@users.noreply.github.com>
2023-05-06 18:50:16 +0100

    created getOperator()

--- CalculatorV1/calculator.py
+++ CalculatorV1/calculator.py
@@ -2,4 +2,10 @@ print(" \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n")
 
 operator = input("1. add\n2. subtract\n3. multiply\n4. divide\nwhat would you like to do? (1/2/3/4): \n")
 
-print(operator)
+
+
+def getOperator():
+    if operator == 1 or 2 or 2 or 4:
+        pass
+    else:
+        input("not a valid option, please try again.")
