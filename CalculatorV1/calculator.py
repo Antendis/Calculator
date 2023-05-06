04798cc172d77495a8eb5b05c655bd73e0c812ed
Antendis <86629893+Antendis@users.noreply.github.com>
2023-05-06 17:45:50 +0100

    init

---
+++ CalculatorV1/calculator.py
@@ -0,0 +1,5 @@
+print(" \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n")
+
+operator = input("1. add\n2. subtract\n3. multiply\n4. divide\nwhat would you like to do? (1/2/3/4): \n")
+
+print(operator)
