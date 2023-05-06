0778651abfcd5ab385c47d20d42f85508ecc9fdb
Antendis <86629893+Antendis@users.noreply.github.com>
2023-05-06 19:16:43 +0100

    Add files via upload

--- CalculatorV1/calculator.py
+++ CalculatorV1/calculator.py
@@ -2,10 +2,11 @@ print(" \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n")
 
 operator = input("1. add\n2. subtract\n3. multiply\n4. divide\nwhat would you like to do? (1/2/3/4): \n")
 
-
-
 def getOperator():
-    if operator == 1 or 2 or 2 or 4:
-        pass
+    if operator == 1 or 2 or 3 or 4:
+        print("success")
     else:
-        input("not a valid option, please try again.")
+        input("not a valid option, please try again: ")
+        getOperator
+
+getOperator()
